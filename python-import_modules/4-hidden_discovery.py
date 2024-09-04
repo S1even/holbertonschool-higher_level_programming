169962a04b457960054afbad9ae3b0e85684cab6
S1even <Stevendevin95520@hotmail.com>
2024-09-04 11:49:11 +0200

    Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+from hidden_4 import *
 
 if __name__ == "__main__":
-    import hidden_4
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for s in dir():
+        if s[:2] != "__":
+            print(s)
