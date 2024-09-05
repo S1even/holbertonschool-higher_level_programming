d814c3dbc04d65cef35f30a1ec973f41389d3580
root <stevendevin95520@hotmail.com>
2024-09-05 08:50:31 +0000

    Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__":
-    for s in dir():
-        if s[:2] != "__":
-            print(s)
+if __name__ == '__main__':
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
