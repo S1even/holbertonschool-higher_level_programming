e1f03cbae76527b512aa25fb62b2113d0f1e63c2
root <stevendevin95520@hotmail.com>
2024-09-05 08:30:45 +0000

    bljbekb

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
         if not i.startswith("__"):
-            print("{}".format(i))
+            print("{}".format(i)
+)
