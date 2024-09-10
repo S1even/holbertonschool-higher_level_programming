34ab39b659b5cd56cb14fc72251d8351701d09ed
S1even <Stevendevin95520@hotmail.com>
2024-09-10 10:47:08 +0200

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
-    roman_to_int_map = {'I': 1, 'V': 5, 'X': 10,
-                'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    prev = 0
-    for letter in reversed(roman_string):
-        value = roman_to_int_map[letter]
-        if value < prev:
-            total -= value
+    sum = 0
+    roman_string += 'I'
+    for i in range(0, len(roman_string) - 1):
+        if roman_string[i] == 'I':
+            if roman_string[i + 1] != 'I':
+                sum -= 1
+            else:
+                sum += 1
+        elif roman_string[i] == 'V':
+            sum += 5
+        elif roman_string[i] == 'X':
+            if roman_string[i + 1] == 'L' or roman_string[i + 1] == 'C':
+                sum -= 10
+            else:
+                sum += 10
+        elif roman_string[i] == 'L':
+            sum += 50
+        elif roman_string[i] == 'C':
+            if roman_string[i + 1] == 'D' or roman_string[i + 1] == 'M':
+                sum -= 100
+            else:
+                sum += 100
+        elif roman_string[i] == 'D':
+            sum += 500
+        elif roman_string[i] == 'M':
+            sum += 1000
         else:
-            total += value
-        prev = value
-    return total
+            return 0
+    return sum
