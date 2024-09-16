75db573691a567912c991ae0203650025fe67512
root <8809@holbertonstudents.com>
2024-09-16 09:01:51 +0000

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a simple Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a geiletric square.
+
+    """
+    def __init__(self, size):
+        """
+        Initializes the square with a specific size.
+
+
+        Args:
+            size (int): The size of the square's side.
+
+        """
+
+        self.__size = size
