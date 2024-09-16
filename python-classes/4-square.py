e55ba7d64b405f802b1aa703bd0e811675e5263f
root <8809@holbertonstudents.com>
2024-09-16 10:14:17 +0000

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -43,13 +43,13 @@ class Square:
             ValueError: Raised if size is less than 0.
         """
 
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """
