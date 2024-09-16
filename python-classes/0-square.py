9a0dbbc6022129ba6d0474b456a38f7e10ab2736
root <8809@holbertonstudents.com>
2024-09-16 08:33:48 +0000

    My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a simple Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a geometric square.
+
+    Currently, it is a placeholder and does not have any properties or methods
+    """
+    pass
