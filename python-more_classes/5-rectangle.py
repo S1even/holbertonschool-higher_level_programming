7a3242c0d1dd2de69f435a9a4d0eecae841b9fdf
S1even <153313481+S1even@users.noreply.github.com>
2024-09-17 12:49:02 +0000

    Detect instance deletion

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -103,7 +103,7 @@ class Rectangle:
 
         """
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         """
         Prints a message when an instance of Rectangle is deleted.
