2d0197e60fe7d2ed489f29d221ffa20bce52f8b5
S1even <153313481+S1even@users.noreply.github.com>
2024-09-17 08:20:21 +0000

    Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    This is a placeholder class definition for a Rectangle.
+    
+    Currently, does not have any properties or methods
+    """
+    pass
