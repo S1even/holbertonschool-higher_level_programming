b996c16c4eada4e2641119e39da521cc497f4e3d
root <8809@holbertonstudents.com>
2024-09-23 09:30:18 +0000

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Contains MyList class
+"""
+
+
+class MyList(list):
+    """
+    This class adds an additional method to display the sorted list
+    """
+    def print_sorted(self):
+        """
+    Method that displays the sorted list,
+        without modifying the original list.
+        """
+        print(sorted(self))
