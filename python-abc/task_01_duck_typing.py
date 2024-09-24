b1565ecfb526c50669f816ae6705d184e5dcf709
S1even <153313481+S1even@users.noreply.github.com>
2024-09-24 13:23:43 +0000

    Shapes, Interfaces, and Duck Typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -24,7 +24,7 @@ class Circle(Shape):
     """Class representing a circle"""
 
     def __init__(self, radius):
-        self._radius = radius
+        self.radius = radius
 
     def area(self):
         """Calculates the area of the circle."""
@@ -34,31 +34,29 @@ class Circle(Shape):
     def perimeter(self):
         """Calculates the perimeter of the circle."""
 
-        return 2 * pi * self._radius
+        return 2 * pi * self.radius
 
 
 class Rectangle(Shape):
     """Class representing a rectangle"""
 
     def __init__(self, width, height):
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     def area(self):
         """Calculate the area of the rectangle."""
 
-        return self._width * self._height
+        return self.width * self.height
 
     def perimeter(self):
         """Calculate the perimeter of the rectangle."""
 
-        return 2 * (self._width + self._height)
+        return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
     """Print the area and perimeter"""
 
-    area = shape.area()
-    perimeter = shape.perimeter()
-    print("Area: {}".format(area))
-    print("Perimeter: {}".format(perimeter))
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
