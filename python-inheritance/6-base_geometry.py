7d13db1c763eddef749a0332c07b3cd2ccd9b05b
S1even <153313481+S1even@users.noreply.github.com>
2024-09-24 09:48:01 +0000

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class.
+    """
+    def area(self):
+        """
+        Raises an Exception with the message: area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
