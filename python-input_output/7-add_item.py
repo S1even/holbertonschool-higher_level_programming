0ba8648de7f6ccde5bf3fb007438f9d311593ee6
root <8809@holbertonstudents.com>
2024-09-30 10:56:52 +0000

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,7 @@ to a JSON file named add_item.json
 """
 from sys import argv
 import os
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
