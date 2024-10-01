5a24fed97cb606a9cfeb95ad91499d020a930ce7
root <8809@holbertonstudents.com>
2024-10-01 08:53:47 +0000

    Basic Serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,37 +1,15 @@
 #!/usr/bin/python3
-import json
+"""0. Basic Serialization"""
+import pickle
 
 
 def serialize_and_save_to_file(data, filename):
-    """
-    Serialize the Python dictionary to a JSON file.
-
-    Parameters:
-    data (dict): The Python dictionary to be serialized.
-    filename (str): The name of the output JSON file.
-    If the file exists, it will be replaced.
-    """
-    try:
-        with open(filename, 'w') as file:
-            json.dump(data, file, indent=4)
-    except Exception as e:
-        print(f"An error occured while writing to the file: {e}")
+    """function that serialize and save data"""
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f)
 
 
 def load_and_deserialize(filename):
-    """
-    Load and deserialize the JSON file to a Python dictionary.
-
-    Parameters:
-    filename (str): The name of the input JSON file.
-
-    Returns:
-    dict: The deserialized Python dictionary.
-    """
-    try:
-        with open(filename, 'r') as file:
-            data = json.load(file)
-            return data
-    except Exception as e:
-        print(f"An error occurred while reading from the file: {e}")
-        return None
+    """function that load and deserialize data"""
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
