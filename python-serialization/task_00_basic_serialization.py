0e122d377296ea7f018a8c2bb6622e88943e7103
root <8809@holbertonstudents.com>
2024-10-01 09:16:07 +0000

    Basic Serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,7 +8,7 @@ import json
 def serialize_and_save_to_file(data, filename):
     """function that serialize and save data"""
     with open(filename, 'w') as json_file:
-        pickle.dump(data, json_file)
+        json.dump(data, json_file)
 
 
 def load_and_deserialize(filename):
