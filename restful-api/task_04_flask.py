90cc0129c223a4c8be4ba09766afd19742fe93d3
S1even <153313481+S1even@users.noreply.github.com>
2024-10-10 09:00:46 +0000

    Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -34,22 +34,24 @@ def get_user(username):
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
+    data = request.get_json()
 
-    rq = request.get_json()
-
-    username = rq.get("username")
-
-    if username:
-        name = rq.get("name")
-        age = rq.get("age")
-        city = rq.get("city")
-
-        users[username] = {"username": username, "name": name, "age": age, "city": city}
-
-        return jsonify({"message": "User added", "user": users[username]}), 201
-
-    else:
+    if "username" not in data:
         return jsonify({"error": "Username is required"}), 400
+
+    username = data["username"]
+
+    users[username] = {
+        "username": username,
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+
+    return jsonify({
+        "message": "User added",
+        "user": users[username]
+    }), 201
 
 
 if __name__ == "__main__":
