8535ed8bf0f7b6e67ced0edc7d8b3cb541776608
root <root@DESKTOP-HLD5I9I.localdomain>
2024-10-29 11:22:32 +0100

    Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.states.id = states.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
