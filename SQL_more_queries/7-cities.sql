92dd825e44020346b33e0ade573282c1b8d9bd5f
root <root@DESKTOP-HLD5I9I.localdomain>
2024-10-29 11:10:30 +0100

    Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates database and Table
+CREATE DATABSE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	state_id INT,
+	name VARCHAR(265),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
