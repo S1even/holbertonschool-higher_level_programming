59793b0d31dbe3ce060dd70e105d5e6d65ea4f5c
root <root@DESKTOP-HLD5I9I.localdomain>
2024-10-29 13:07:00 +0100

    Only Comedy

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Import Database Dump
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_show.id = tv_show_genres.show.id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show.id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_show.title
