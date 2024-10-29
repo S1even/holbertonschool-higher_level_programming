0d887524f9c72ddb2b8ad14bac33ffe4837ce118
S1even <8809@holbertonstudents.com>
2024-10-29 15:44:22 +0100

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    results = cursor.fetchall()
+    rows = cursor.fetchall()
 
     for row in rows:
         print(row)
