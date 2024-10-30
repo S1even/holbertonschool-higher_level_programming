584216c187b5538ba996dcfa8fe08133fc0a7c6c
S1even <153313481+S1even@users.noreply.github.com>
2024-10-30 13:06:15 +0000

    Update code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-
 """
 Return matching states
 parameters given to script: username, password, database, state to match
 """
 
-import sys
+from sys import argv
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states \
-        WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
-    rows = cursor.fetchall()
+if __name__ == '__main__':
 
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
+        '{}' ORDER BY states.id".format(argv[4]))
+
+    rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
