8bbf5e0e7a919b00edb66e1679bba0e5dd8bb2ac
S1even <153313481+S1even@users.noreply.github.com>
2024-10-30 13:31:31 +0000

    Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+return info from both tables (tables 'cities' 'states)
+parameters given to script: username, password, database
+"""
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
