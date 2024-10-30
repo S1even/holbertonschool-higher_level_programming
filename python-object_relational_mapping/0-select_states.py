8d1b736fe237121ef1e9890a9cc531bb2c3bf25b
S1even <153313481+S1even@users.noreply.github.com>
2024-10-30 13:09:47 +0000

    Chmod + pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,20 +4,25 @@ lists all table values
 parameters given to script: username, password, database
 """
 
+import sys
 import MySQLdb
-from sys import argv
+
 
 if __name__ == "__main__":
-
-    db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cursor.fetchall():
+    rows = cursor.fetchall()
+
+    for row in rows:
         print(row)
+
     cursor.close()
     db.close()
