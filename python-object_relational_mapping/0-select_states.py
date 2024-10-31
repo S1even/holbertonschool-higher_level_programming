b596f3fc0c2e884c370d2de19c65802a9780c04b
S1even <153313481+S1even@users.noreply.github.com>
2024-10-31 09:34:31 +0000

    chmod

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,48 @@
 #!/usr/bin/python3
-"""
-lists all table values
-parameters given to script: username, password, database
-"""
 
+"""
+This module connects to a MySQL database and retrieves all states
+from the specified database. The states are sorted in ascending order
+by their IDs and displayed in a specific format.
+"""
 
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    connection = MySQLdb.connect(
-        host='localhost',
+def main():
+    """
+    The main function that executes the logic to connect to the MySQL
+    database and retrieve states. It takes three command-line arguments:
+    MySQL username, password, and database name. It connects to the
+    database and executes a SQL query to select and print all states
+    sorted by their ID in ascending order.
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+    cursor = db.cursor()
 
-    for row in rows:
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+    results = cursor.fetchall()
+
+    for row in results:
         print(row)
 
     cursor.close()
-    connection.close
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
